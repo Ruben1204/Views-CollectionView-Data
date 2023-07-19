53bbae260d3e686cbd0cb38662040d5027015168
Ruben Darío Rueda <ingeniero.rubenrueda@gmail.com>
2023-07-19 18:20:07 -0500

    FirstCommit
    implementation CollectionView and your Propertys of interaction with Data

---
+++ Views/CollectionView-Vista.xaml.cs
@@ -0,0 +1,14 @@
+namespace ControlesTipoVista.Views;
+
+public partial class CollectionView_Vista : ContentPage
+{
+	public CollectionView_Vista()
+	{
+		InitializeComponent();
+	}
+
+    private void CollectionView_RemainingItemsThresholdReached(object sender, EventArgs e)
+    {
+
+    }
+}
